c7080ef9737093b824c75a27fff4aba9c3702c0c
Drissdo185 <dothanhdat185@gmail.com>
2025-10-22 13:27:31 +0700

    remove files

--- app/processor/test.py
+++ app/processor/test.py
@@ -9,7 +9,7 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(project_root))
 
-from app.ingest.factory import ProcessorFactory
+from app.processor.factory import ProcessorFactory
 
 async def main():
     # Get PDF path
